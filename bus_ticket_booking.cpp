f402d1139a120932932ed646c15d6724c95713a3
Abdus Salam Talukder <salamtalukder.official@gmail.com>
2026-05-24 17:25:15 +0600

    final version

--- bus_ticket_booking.cpp
+++ bus_ticket_booking.cpp
@@ -1,34 +1,221 @@
 #include <iostream>
 #include <string>
+#include <limits> 
 using namespace std;
 
+
+//total bus :
+int busCount = 5;
+
+//bus route and fare: 
+string busRoute[100];
+int busFare[100];
+
+int seats[100][40];   // 100 karon jodi aro bus add kori
+string passenger[100][40]; // passenger name;
+
+// set the bus information
+void initBuses() {
+    busRoute[0] = "Dhaka-Sylhet";
+    busFare[0] = 500;
+
+    busRoute[1] = "Dhaka-CoxBazar";
+    busFare[1] = 1200;
+
+    busRoute[2] = "Dhaka-Chittagong";
+    busFare[2] = 700;
+
+    busRoute[3] = "Dhaka-Rajshahi";
+    busFare[3] = 600;
+
+    busRoute[4] = "Dhaka-Khulna";
+    busFare[4] = 800;
+}
+
+// set the seat information
+void initSeats() {
+    for (int i = 0; i < busCount; i++) {
+        for (int j = 0; j < 40; j++) {
+            seats[i][j] = 0;
+            passenger[i][j] = "";
+        }
+    }
+}
+
 void viewBuses() {
-    cout << "Viewing buses...\n";
-    cout << "This feature will be added soon!\n";
+    cout << "\n--- Available Buses ---\n";
+    for (int i =0; i<busCount; i++) {
+        cout << "Bus ID: " << i << endl;
+        cout << "Route : " << busRoute[i] << endl;
+        cout << "Fare  : " << busFare[i] << " BDT" << endl;
+        cout << endl;
+    }
+    cout << "\nPress Enter to go back  ";
+
+    cin.ignore();
+    cin.get(); 
 }
 
 void viewSeats() {
-    cout << "Viewing seats...\n";
-    cout << "This feature will be added soon!\n";
+    int bus_id;
+
+      cout << "\n---Bus list---\n";
+    for (int i =0; i<busCount; i++) {
+        cout << "Bus ID: " << i << endl;
+        cout << "Route : " << busRoute[i] << endl;
+        cout << "Fare  : " << busFare[i] << " BDT" << endl;
+        cout << endl;
+    }
+    cout << "Enter Bus ID to choose seat: ";
+    cin >> bus_id;
+
+    if (bus_id < 0 || bus_id >= busCount) {
+        cout << "Invalid Bus ID!\n";
+        return;
+    }
+
+    cout << "\nBus: " << busRoute[bus_id] << endl;
+    
+     for (int i = 0; i < 40; i++) {
+
+        if (seats[bus_id][i] == 0)
+            cout << "[ " << i + 1 << " ]\t";
+        else
+            cout << "[ X ] ";
+
+        if ((i + 1) % 4 == 0)
+            cout << endl;
+    }
+
+    cout << "\n\n";
+    cout << "X = Booked Seat\n";
+
+    cout << "\nPress Enter to go back...";
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cin.get();
+
 }
 
 void bookTicket() {
-    cout << "Booking ticket...\n";
-    cout << "This feature will be added soon!\n";
+    int bus_id;
+    int seat_no;
+    string name;
+
+    cout << "\nEnter Bus ID: ";
+    cin >> bus_id;
+
+    if (bus_id < 0 || bus_id >= busCount) {
+        cout << "Invalid Bus ID!\n";
+        return;
+    }
+
+     cout << "\nSeats for Bus " << busRoute[bus_id] << ":\n";
+
+    for (int i = 0; i < 40; i++) {
+
+        if (seats[bus_id][i] == 0)
+            cout << "[ " << i + 1 << " ] ";
+        else
+            cout << "[ X ] ";
+
+        if ((i + 1) % 4 == 0)
+            cout << endl;
+    }
+
+    // now seat input
+    cout << "\nEnter Seat Number (1-40): ";
+    cin >> seat_no;
+
+    if (seat_no < 1 || seat_no > 40) {
+        cout << "Invalid Seat Number!\n";
+        return;
+    }
+
+    // check if the seat is booked or not
+    if (seats[bus_id][seat_no - 1] == 1) {
+        cout << "Seat already booked!\n";
+        return;
+    }
+
+    // booking
+    cout << "Enter Passenger Name(only surname): ";
+    cin >> name;
+
+    seats[bus_id][seat_no - 1] = 1;
+    passenger[bus_id][seat_no - 1] = name;
+
+    cout << "\nBooking Successful!\n";
+    cout << "Seat No: " << seat_no << endl;
+    cout << "Passenger: " << name << endl;
+
+    cout << "\nPress Enter to continue...";
+
+    
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cin.get();
 }
 
 void cancelTicket() {
-    cout << "Cancelling ticket...\n";
-    cout << "This feature will be added soon!\n";
+    int B_id; // Bus ID
+    int S_no; // Seat number
+
+    cout << "Enter Bus ID: ";
+    cin >> B_id;
+
+    if (B_id < 0 || B_id >= busCount) {
+        cout << "Invalid Bus ID!\n";
+        return;
+    }
+
+    cout << "Enter Seat Number (1-40): ";
+    cin >> S_no;
+
+    if (S_no < 1 || S_no > 40) {
+        cout << "Invalid Seat Number!\n";
+        return;
+    }
+
+    if (seats[B_id][S_no - 1] == 0) {
+        cout << "Seat is already empty!\n";
+        return;
+    }
+
+    seats[B_id][S_no - 1] = 0;
+    passenger[B_id][S_no - 1] = "";
+
+    cout << "\nTicket Cancelled Successfully!\n";
+    cout << "Bus: " << busRoute[B_id] << endl;
+    cout << "Seat No: " << S_no << endl;
 }
 
 void searchBooking() {
-    cout << "Searching booking...\n";
-    cout << "This feature will be added soon!\n";
+    string name;
+    bool found = false;
+
+    cout << "\nEnter Passenger Name: ";
+    cin >> name;
+
+    for (int i = 0; i < busCount; i++) {
+        for (int j = 0; j < 40; j++) {
+
+            if (passenger[i][j] == name) {
+
+                cout << "\nBooking Found!\n";
+                cout << "Bus: " << busRoute[i] << endl;
+                cout << "Bus ID: " << i << endl;
+                cout << "Seat No: " << j + 1 << endl;
+
+                found = true;
+            }
+        }
+    }
+
+    if (!found) {
+        cout << "No booking found for this name!\n";
+    }
 }
 
 void addBus() {
-    cout << "Adding bus...\n";
     cout << "This feature will be added soon!\n";
 }
 
@@ -133,6 +320,9 @@ int main() {
 
     string user_name = "user";
     int user_pin = 9999;
+    
+    initBuses(); // load out all buses
+    initSeats(); // loaded all seats
 
     int choice;
 
